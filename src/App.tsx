cb19304bcddabf5e3fa1d326bae0c9a54fda5d45
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-12-29 21:16:48 +0900

    feat: add lazy and Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import history from './plugins/history';
-import Home from './container/index';
-import Series from './container/series';
-import Book from './container/book';
 import Header from './components/header';
+const Home = lazy(() => import('./container/index'));
+const Series = lazy(() => import('./container/series'));
+const Book = lazy(() => import('./container/book'));
 
 const App: React.FC = () => {
   return (
     <>
       <div>
-        <Header />
         <Router history={history}>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/series/:seriesId" component={Series} />
-            <Route exact path="/story/:seriesId" component={Book} />
-          </Switch>
+          <Header />
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/series/:seriesId" component={Series} />
+              <Route exact path="/story/:seriesId" component={Book} />
+            </Switch>
+          </Suspense>
         </Router>
       </div>
     </>
