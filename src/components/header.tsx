f5213cf5f6897e0e8036606d5e802c8ebea41a89
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-01-12 23:01:26 +0900

    feat: add Link to top

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
-    <>
-      <header>
-        <Title>MANGA APP</Title>
-      </header>
-    </>
+    <header>
+      <Title>
+        <Link to={`/`}>MANGA APP</Link>
+      </Title>
+    </header>
   );
 };
 
 const Title = styled.h1`
   padding: 10px 30px;
   margin: 0;
+  a {
+    color: #000;
+  }
 `;
 
 export default Header;
